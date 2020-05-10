367789a165ad9c36a939196cde6112b685ab1e03
HuuNgan <ngan.luongbkdev@hcmut.edu.vn>
2020-05-10 09:36:29 +0700

    add cmt to lab1_tn2.asm

--- lab1 - 2/lab1_TN2.asm
+++ lab1 - 2/lab1_TN2.asm
@@ -1,14 +1,14 @@
 ; tao xung 1Hz, duty cycle = 50%
 ORG 2000H
 MAIN:
-	   CPL P1.0
+       CPL P1.0
 ;DELAY 500ms
-       MOV R7, #4
-LOOP2: MOV R6, #250
-LOOP1: MOV R5, #250
-       DJNZ R5, $
-	   DJNZ R6, LOOP1
-	   DJNZ R7, LOOP2
+       MOV R7, #4          ;1MC
+LOOP2: MOV R6, #250        ;R7*1MC
+LOOP1: MOV R5, #250        ;R6*R7*1MC
+       DJNZ R5, $          ;R5*R6*R7*2MC
+       DJNZ R6, LOOP1      ;R6*R7*2MC
+       DJNZ R7, LOOP2      ;R7*2MC
        
-	   SJMP MAIN
+       SJMP MAIN           ;2MC
 END
