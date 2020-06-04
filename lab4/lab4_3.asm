582a0c4b05588ab68ddb55f19df7b0d75c6f86fa
HuuNgan <ngan.luongbkdev@hcmut.edu.vn>
2020-06-04 13:23:27 +0700

    add delay5ms to lab4_3

--- lab4/lab4_3.asm
+++ lab4/lab4_3.asm
@@ -1,13 +1,14 @@
 ;Chong rung nut nhan, nhan lan 1 led sang, nhan tiep led tat
 
-COUNT EQU -50000
+COUNT1 EQU -50000
+COUNT2 EQU -5000
 ORG 2000H
     MOV TMOD, #01H    ;Chon che do 1, su dung timer0
 MAIN:
     SETB P1.1
-LOOP_FOREVER:
-    JB P1.0, $
+
 LOOP:
+    JB P1.0, $
     MOV C, P1.0
     ACALL DELAY
     ANL C,/P1.0
@@ -19,11 +20,23 @@ LOOP:
     
     CPL P1.1
     JNB P1.0, $
-    SJMP LOOP_FOREVER
+    ACALL DELAY5ms
+    SJMP LOOP
 ;Delay 50ms ***********************************************
 DELAY:
-    MOV TH0, #HIGH(COUNT)
-    MOV TL0, #LOW(COUNT)
+    MOV TH0, #HIGH(COUNT1)
+    MOV TL0, #LOW(COUNT1)
+    SETB TR0
+    JNB TF0, $
+    CLR TR0
+    CLR TF0
+RET
+;End Delay ************************************************
+
+;Delay 5ms ***********************************************
+DELAY5ms:
+    MOV TH0, #HIGH(COUNT2)
+    MOV TL0, #LOW(COUNT2)
     SETB TR0
     JNB TF0, $
     CLR TR0
